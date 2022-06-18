b0ff2d549f576dbbff510d09d5dd0ed598132a4b
BIYA Paul <paul@workerly.io>
2022-06-18 10:23:30 +0100

    feat: Setting up Cookies.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createHead } from '@vueuse/head'
 
+import { globalCookiesConfig } from 'vue3-cookies'
 import { store } from '@/store'
 import router from '@/routes'
 import i18n from '@/locales'
@@ -8,5 +9,13 @@ import App from './App.vue'
 import './main.css'
 
 const head = createHead()
+
+globalCookiesConfig({
+  expireTimes: '30d',
+  path: '/',
+  domain: '',
+  secure: true,
+  sameSite: 'None',
+})
 
 createApp(App).use(head).use(i18n).use(router).use(store).mount('#app')

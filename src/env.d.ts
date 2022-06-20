fa0873b92edfe9f1c7b005644109ee1767f3e2f6
BIYA Paul <paul@workerly.io>
2022-06-20 14:24:16 +0100

    chore: Cleanups.

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,11 +2,11 @@
 
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
   const component: DefineComponent<{}, {}, any>
   export default component
 }
 
+// eslint-disable-next-line
 interface ImportMetaEnv {
   VITE_BASE_URL: string
 }
